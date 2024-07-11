8c52fa958a33d5060b837f85fbc91d5fd6f7e80b
Avnolo <m.hasnawi@avnology.com>
2024-07-11 12:13:07 +0300

    Update to api

--- generate-qrcode.php
+++ generate-qrcode.php
@@ -1,10 +1,18 @@
 <?php
+// Allow CORS from any origin (or specify the frontend origin)
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit; // Exit for preflight request
+}
+
 // Include the QR code generator library
 include "phpqrcode-master/qrlib.php";
 
-// Function to generate QR code
-function generateQRCode($data) {
-    // Format data as vCard
+// Function to generate vCard QR code
+function generateVCardQRCode($data) {
     $vcard = "BEGIN:VCARD\n";
     $vcard .= "VERSION:3.0\n";
     $vcard .= "FN:" . $data['first_name'] . " " . $data['last_name'] . "\n";
@@ -16,33 +24,57 @@ function generateQRCode($data) {
     }
     $vcard .= "END:VCARD";
 
-    // Generate a unique filename for the QR code image
-    $filename = uniqid() . '.png';
-
-    // Generate QR code and save it to a file
-    QRcode::png($vcard, $filename);
-
-    // Return the filename
+    $filename = time() . '_' . uniqid() . '.png';
+    QRcode::png($vcard, $filename, QR_ECLEVEL_Q, 10);
     return $filename;
 }
 
-// Check if form data is received
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Form data
-    $data = array(
-        'first_name' => $_POST['first_name'],
-        'last_name' => $_POST['last_name'],
-        'phone_number' => $_POST['phone_number'],
-        'job_title' => $_POST['job_title'],
-        'email' => $_POST['email'],
-        'social_media' => explode("\n", $_POST['social_media'])
-    );
+// Function to generate link QR code
+function generateLinkQRCode($data) {
+    $filename = time() . '_' . uniqid() . '.png';
+    QRcode::png($data, $filename, QR_ECLEVEL_L, 10);
+    return $filename;
+}
 
-    // Generate QR code
-    $qrCodeFilename = generateQRCode($data);
+// Function to delete old QR code files
+function deleteOldQRCodeFiles($directory, $expiration = 10) {
+    $files = glob($directory . '/*.png');
+    $now = time();
 
-    // Return the filename as a response
-    echo $qrCodeFilename;
-    exit; // Stop further execution
+    foreach ($files as $file) {
+        if (is_file($file)) {
+            $file_creation_time = filectime($file);
+            if ($now - $file_creation_time >= $expiration) {
+                unlink($file); // Delete the file
+            }
+        }
+    }
+}
+
+// Register a shutdown function to delete old files after script execution
+register_shutdown_function('deleteOldQRCodeFiles', __DIR__);
+
+// Read JSON input
+$input = json_decode(file_get_contents('php://input'), true);
+
+if ($input) {
+    if ($input['type'] === 'vcard') {
+        $data = $input['data'];
+        $data['social_media'] = explode("\n", $data['social_media']);
+        $filename = generateVCardQRCode($data);
+    } elseif ($input['type'] === 'link') {
+        $data = $input['data'];
+        $filename = generateLinkQRCode($data);
+    } else {
+        http_response_code(400);
+        echo json_encode(["error" => "Invalid type"]);
+        exit;
+    }
+
+    // Return the filename as a JSON response
+    echo json_encode(["filename" => $filename]);
+} else {
+    http_response_code(400);
+    echo json_encode(["error" => "Invalid input"]);
 }
 ?>
